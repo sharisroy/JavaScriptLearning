0265411f583b204b4b20802dc3f2abbdeed783fa
Haris <haris.chandra@pathaopay.com>
2023-01-09 13:09:11 +0600

    Receive data from another script File

---
+++ applicationAPI.js
@@ -0,0 +1,11 @@
+
+// const name = require("./main.js")
+// const PI = require("./main.js")
+// const addNumbers = require("./main.js")
+
+const {name, PI, addNumbers} = require("./mainScript.js")
+
+console.log(name); // Prints: Haris Chandra Roy
+console.log(PI); // Prints: 3.14
+console.log(addNumbers(5, 16)); // Prints: 21
+
---
+++ mainScript.js
@@ -0,0 +1,12 @@
+let name = "Haris Chandra Roy";
+const PI = 3.14; 
+ 
+function addNumbers(a, b){
+    return a + b;
+}
+ 
+// Exporting variables and functions
+// module.exports = name
+// module.exports = PI;
+// module.exports = addNumbers
+module.exports = {name , PI, addNumbers}
